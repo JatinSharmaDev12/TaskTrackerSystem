f8e2d70abf567b71b609f23db8fd2fba1870d3db
jatin sharma <jatinsharma1@axeno.co>
2026-02-11 19:15:03 +0530

    add dto for ImportData used as a wrapper for json

---
+++ src/main/java/com/axeno/tasktrackerapp/dto/ImportDataWrapper.java
@@ -0,0 +1,11 @@
+package com.axeno.tasktrackerapp.dto;
+
+import com.axeno.tasktrackerapp.model.Individual;
+import com.axeno.tasktrackerapp.model.Program;
+
+import java.util.List;
+
+public class ImportDataWrapper {
+    private List<Individual> individuals;
+    private List<Program> programs;
+}
